4932ff29cbd05f09ec9fed74bc81d604ad2fb1f1
Jason Snider <root@jasonsnider.com>
2019-10-04 09:41:33 -0500

    Proof of concept - DB

--- webroot/articles/index.py
+++ webroot/articles/index.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python
 import sys
 sys.path.append("../..")
+from lib import template
 
 import cgi
 import cgitb
 cgitb.enable()
-
-from lib import template
+import pymysql
 
 title = 'Blog'
 body = '<h1>Jason Snider\'s Blog</h1>'
 
 print('Content-type: text/html\n\n')
+
+
+
+# Connect to the database.
+
+conn = pymysql.connect(
+    db='jasonsnider',
+    user='root',
+    passwd='password',
+    host='localhost')
+c = conn.cursor()
+
+c.execute("SELECT * FROM articles")
+
+for r in c.fetchall():
+    body = body + '''\
+<a href="/articles/view/{slug}">
+    <h2>{title}</h2>
+    <p>{description}</p>
+</a><hr>\
+'''.format(
+    slug=r[1],
+    title=r[2],
+    description=r[4]
+)
+
+
 print(template.input(title, body))
